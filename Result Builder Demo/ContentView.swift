035761de986f95bc567ec6df1f00d1ff48596964
P. Nam <phuongnamp99@gmail.com>
2023-01-23 13:26:00 +0700

    Initial Commit

---
+++ Result Builder Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Result Builder Demo
+//
+//  Created by pnam on 23/01/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Result Builder Demo/Result_Builder_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Result_Builder_DemoApp.swift
+//  Result Builder Demo
+//
+//  Created by pnam on 23/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct Result_Builder_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
